5ed14a35a40e29b33b159738e532ad4954db3b94
Grolaf <75337003+Grolaf@users.noreply.github.com>
2021-03-21 21:26:07 +0100

    Update Controleur.java

--- src/main/java/genconf/controleur/Controleur.java
+++ src/main/java/genconf/controleur/Controleur.java
@@ -1,81 +1,161 @@
-package genconf.controleur;
+package modele;
 
-import genconf.modele.GenConf;
-import genconf.vue.IHM;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Controleur {
 
+public class Conference implements Serializable {
+
+    private static final long serialVersionUID = 1L;  // nécessaire pour la sérialisation
     private final GenConf genconf;
-    private final IHM ihm;
+    private String nom;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private Map<String, Utilisateur> administrateurs;  // association qualifiée par l'email
+    private Map<String, TypeCommunication> typeCommunication;
+    private Map<String, Communication> communication;
+    private Map<String, Track> track;
+    private Map<String, Session> session;
+    private Map<String, Utilisateur> inscrit;
+    private String theme;
+    private String lieu;
+    private LocalDate dateT1;
+    private LocalDate dateT2;
+    private LocalDate dateT3;
+    private LocalDate dateT4;
+    private String logo;
+    private String texteAccueil;
+        
+    
+    // Invariant de classe : !dateDebut.isAfter(dateFin)
+    //     On utilise la négation ici pour exprimer (dateDebut <= dateFin), ce
+    //     qui est équivalent à !(dateDebut > dateFin).
 
-    public Controleur(GenConf genconf) {
+    
+
+    public Conference(GenConf genconf, String nom, LocalDate dateDebut, LocalDate dateFin) {
+        assert !dateDebut.isAfter(dateFin);
         this.genconf = genconf;
-
-        // choisir la classe CLI ou GUI
-//        this.ihm = new CLI(this);
-        this.ihm = new IHM(this);
+        this.nom = nom;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        
     }
 
-    public void demarrer() {
-        this.ihm.afficherInterface();
+    public void setAdmin(Utilisateur u){
+        administrateurs.add(u.email,u);
+        u.addConferenceAdministree(this);
+    }
+    public String getNom() {
+        return this.nom;
     }
 
-    public void gererDialogue(Commande cmd) {
-        switch (cmd) {
-            case QUITTER:
-                break;
-            case CREER_UTILISATEUR:
-                this.ihm.saisirUtilisateur();
-                break;
-            case CREER_CONFERENCE:
-                this.ihm.saisirNouvelleConference(this.genconf.getConferences().keySet());
-                break;
-            default:
-                assert false : "Commande inconnue.";
-        }
+    public LocalDate getDateDebut() {
+        return dateDebut;
     }
 
-    public void creerUtilisateur(IHM.InfosUtilisateur infos) {
-        boolean nouvelUtilisateur = this.genconf.ajouteUtilisateur(
-                infos.email,
-                infos.nom,
-                infos.prenom
-        );
-        if (nouvelUtilisateur) {
-            this.ihm.informerUtilisateur(
-                    "Nouvel·le utilisa·teur/trice : " + infos.prenom + " " + infos.nom + " <" + infos.email + ">",
-                    true
-            );
-        } else {
-            this.ihm.informerUtilisateur(
-                    "L'utilisa·teur/trice " + infos.email + " est déjà connu·e de GenConf.",
-                    false
-            );
-        }
+    public void setDateDebut(LocalDate dateDebut) {
+        assert !dateDebut.isAfter(this.dateFin);
+        this.dateDebut = dateDebut;
     }
 
-    public void creerConference(IHM.InfosNouvelleConference infos) {
-        // création d'un Utilisateur si nécessaire
-        boolean nouvelUtilisateur = this.genconf.ajouteUtilisateur(
-                infos.admin.email,
-                infos.admin.nom,
-                infos.admin.prenom
-        );
-        if (nouvelUtilisateur) {
-            this.ihm.informerUtilisateur("Nouvel·le utilisa·teur/trice : " + infos.admin.prenom + " " + infos.admin.nom + " <" + infos.admin.email + ">",
-                    true
-            );
-        }
-
-        this.genconf.nouvelleConference(
-                infos.nom,
-                infos.dateDebut,
-                infos.dateFin,
-                infos.admin.email
-        );
-        this.ihm.informerUtilisateur(
-                "Nouvelle conférence : " + infos.nom + ", administrée par " + infos.admin.email,
-                true
-        );
+    public LocalDate getDateFin() {
+        return dateFin;
     }
+
+    public void setDateFin(LocalDate dateFin) {
+        assert !this.dateDebut.isAfter(dateFin);
+        this.dateFin = dateFin;
+    }
+    
+    
+    public Collection <Utilisateur> getAdmin(){
+        return this.administrateurs.values();
+    } 
+
+    public void ajouteAdministrateur(Utilisateur admin) {
+        assert !this.administrateurs.containsKey(admin.getEmail());
+        this.administrateurs.put(admin.getEmail(), admin);
+        admin.ajouteConferenceAdministree(this);
+    }
+    public boolean addCommunication(Communication communication, int IDcomm){
+
+    }
+
+    public void addInscrit(Utilisateur inscrit, String email){
+
+    }
+    public void addSession(Session session){
+
+    }
+
+    public void addTypeCommunication(TypeCommunication type){
+
+    }
+    public Communication getCommunication(int numeroComm){
+
+    }
+    public Map<int,Communication> getCommunications(){
+
+    }
+    public String getDates(){
+
+    }
+    public Session getSession(String intituleSession){
+
+    }
+    public Map<String,Sessions> (){
+
+    }
+    public TypeCommunication (String libelle){
+
+    }
+    public Map<String,Session>(){
+
+    }
+    public boolean is SalleDisponible(String salle, Session session){
+
+    }
+
+    public void setAdminConf(Utilisateur utilisateur, String email){
+
+    }
+    public void setDateT1(String dateT1){
+
+    }
+    public void setDateT2(String dateT2){
+
+    }
+    public void setDateT3(String dateT3){
+
+    }
+    public void setDateT4(String dateT4){
+
+    }
+    public void setLieu(String lieu){
+
+    }
+    public void setLogo(String logo){
+
+    }
+    public void setTexteAccueil(String texteAccueil){
+
+    }
+    public void setTheme(String theme){
+
+    }
+    public boolean supprimerCommunication(Communication communication){
+
+    }
+    public boolean supprimerSession(String intituleSession){
+
+    }
+    public void supprimerTypeCommunication(String libelle){
+
+    }
+
+
 }
