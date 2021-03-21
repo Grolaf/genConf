370a570b876ea2c6d5627ba8c4be3de8a8bd9708
Grolaf <75337003+Grolaf@users.noreply.github.com>
2021-03-21 21:24:13 +0100

    Update Controleur.java

--- src/main/java/genconf/controleur/Controleur.java
+++ src/main/java/genconf/controleur/Controleur.java
@@ -1,7 +1,9 @@
 package genconf.controleur;
 
 import genconf.modele.GenConf;
+import genconf.*;
 import genconf.vue.IHM;
+import java.util.Iterator;
 
 public class Controleur {
 
@@ -16,66 +18,72 @@ public class Controleur {
         this.ihm = new IHM(this);
     }
 
-    public void demarrer() {
-        this.ihm.afficherInterface();
+    public void addCommunicationASession(int numeroCommunication, String intituleSession, Conference conference) {
+        Session session = conference.getSession(intituleSession);
+        Communication communication = conference.getCommunication(numeroCommunication);
+        communication.setSession(session);
+    }
+    
+    public void consulterMetadonneesConference(Conference conference) {
+        ihm.afficherInformationsConf(conference);
+    }
+    
+    public void creerConference() {
+        
+    }
+    
+    public Communication creerCommunication(Conference conference) {
+    	Map<String, Communication> communications = genConf.getCommunications(conference.getNomConf());
+    	ihm.afficherCommunications(communications);
+    	
+    	String titre = ihm.getTitreCommunication();
+    	Iterator<String> it = communications.keySet().iterator();
+    	while(it.hasNext()) {
+    		Communication c = communications.get(it.next());
+    		if (c.getTitre() == titre) {
+    			ihm.notifier("Cette communication existe déjà");
+    			return NULL;
+    		}
+    	}
+    	
+    	String nom = ihm.saisirNomUtilisateur();
+    	String prenom = ihm.saisirPrenomUtilisateur();
+    	String mail = ihm.saisirMailUtilisateur();
+    	
+    	Utilisateur u = conference.getUtilisateur(mail, nom, prenom);
+    	if(!u) {
+    		u = new Utilisateur(mail, prenom, nom);
+    	}
+    	
+    	Communication communication = new Communication(u);
+    	communication.setTypeCommunication(conference.getTypeCommunication(ihm.getNomTypeCommunication()));
+    	communication.setTitre(ihm.getTitreCommunication());
+    	communication.addAuteur(ihm.getAuteursCommunication());
+    	communication.setLienPDF(ihm.saisirLienPDF());
+    	communication.setLienVideo(ihm.saisirLienVideo());
+    	communication.setDate(ihm.saisirDate());
+    	communication.setHoraires(ihm.saisirHeure(), ihm.saisirHeure());
+    	
+    	
+    	int IDcomm = this.genererIDcommunication();
+    	conference.addCommunication(communication, IDcomm);
+
     }
 
-    public void gererDialogue(Commande cmd) {
-        switch (cmd) {
-            case QUITTER:
-                break;
-            case CREER_UTILISATEUR:
-                this.ihm.saisirUtilisateur();
-                break;
-            case CREER_CONFERENCE:
-                this.ihm.saisirNouvelleConference(this.genconf.getConferences().keySet());
-                break;
-            default:
-                assert false : "Commande inconnue.";
-        }
-    }
+    public Utilisateur creerCompteGenConf() {
+    	String nom = ihm.saisirNomUtilisateur();
+    	String prenom = ihm.saisirPrenomUtilisateur();
+    	String mail = ihm.saisirMailUtilisateur();
 
-    public void creerUtilisateur(IHM.InfosUtilisateur infos) {
-        boolean nouvelUtilisateur = this.genconf.ajouteUtilisateur(
-                infos.email,
-                infos.nom,
-                infos.prenom
-        );
-        if (nouvelUtilisateur) {
-            this.ihm.informerUtilisateur(
-                    "Nouvel·le utilisa·teur/trice : " + infos.prenom + " " + infos.nom + " <" + infos.email + ">",
-                    true
-            );
-        } else {
-            this.ihm.informerUtilisateur(
-                    "L'utilisa·teur/trice " + infos.email + " est déjà connu·e de GenConf.",
-                    false
-            );
-        }
+    	Utilisateur u = genconf.getUtilisateur(nom, prenom, mail);
+    	if (u) {
+    		ihm.notifier("Le compte existe déjà");
+    	} else {
+    		u = new Utilisateur(mail, nom, prenom);
+    		genconf.addCompteConf(nom, prenom, mail);
+    		ihm.notifier("Le compte a été créé");
+    	}
     }
-
-    public void creerConference(IHM.InfosNouvelleConference infos) {
-        // création d'un Utilisateur si nécessaire
-        boolean nouvelUtilisateur = this.genconf.ajouteUtilisateur(
-                infos.admin.email,
-                infos.admin.nom,
-                infos.admin.prenom
-        );
-        if (nouvelUtilisateur) {
-            this.ihm.informerUtilisateur("Nouvel·le utilisa·teur/trice : " + infos.admin.prenom + " " + infos.admin.nom + " <" + infos.admin.email + ">",
-                    true
-            );
-        }
-
-        this.genconf.nouvelleConference(
-                infos.nom,
-                infos.dateDebut,
-                infos.dateFin,
-                infos.admin.email
-        );
-        this.ihm.informerUtilisateur(
-                "Nouvelle conférence : " + infos.nom + ", administrée par " + infos.admin.email,
-                true
-        );
-    }
+    
+    
 }
