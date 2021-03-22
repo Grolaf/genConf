7c0e20ef175c3afb2b01bcec76c495279f86061f
Magestia <fastemail.flyn@gmail.com>
2021-03-22 15:35:12 +0000

    wallah je vais vous goomer, marquez les chose

---
+++ src/main/java/genconf/modele/Couleur.java
@@ -0,0 +1,23 @@
+package modele;
+
+/* Classic méthode
+public enum Couleur {
+  NOIR,ORANGE,VERT,BLUE,ROUGE,JAUNE,MAGENTA
+};
+*/ 
+
+public enum Couleur{
+
+  NOIR, ORANGE, VERT, BLUE, ROUGE, JAUNE,MAGENTA;
+
+
+  private Couleur(Couleur couleur){
+    this.couleur=couleur;
+  }
+  public Couleur getCouleur(Couleur couleur) {
+      return this.couleur=couleur;
+  })
+
+  
+
+}
---
+++ src/main/java/genconf/modele/TypeCommunication.java
@@ -0,0 +1,48 @@
+package modele; 
+
+import java.time.LocalDate;
+
+
+public class TypeCommunication(){
+
+private String libelle;
+private boolean videoObligatoire;
+private boolean pdfObligatoire;~
+
+ // Constructeur : 
+    public TypeCommunication(String libelle, boolean videoObligatoire, boolean pdfObligatoire) {
+
+     this.libelle=libelle;
+     this.videoObligatoire=videoObligatoire;
+     this.pdfObligatoire=pdfObligatoire;
+     
+    }
+
+  // Getters :
+
+  public boolean getLienPDFObligatoire(){
+    return this.LienPDFObligatoire
+  }
+
+  public TypeCommunication getTypeCommunication(String libelle){
+    return this.libelle=libelle;
+
+  }
+
+  // Setters
+
+  public void setLibelle(String libelle){
+     this.libelle=libelle:
+  }
+
+  public void setpdfObligatoire(boolean pdfObligatoire){
+        this.pdfObligatoire=pdfObligatoire;
+  }
+
+  public void setVideoObligatoire(boolean videoObligatoire){
+        this.videoObligatoire=videoObligatoire;
+  }
+
+  // Destruceur ? 
+
+}
