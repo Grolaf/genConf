5f51e7f1c1b392aec1c1d3e47a66d5a097051f61
Alexandre <alexandre.bouijoux@gmail.com>
2021-03-24 16:23:12 +0100

    Utilisateur ok

--- src/main/java/genconf/modele/Utilisateur.java
+++ src/main/java/genconf/modele/Utilisateur.java
@@ -12,58 +12,63 @@ public class Utilisateur implements Serializable {
     private final String email;
     private String nom;
     private String prenom;
-    private final Map<String, Conference> conferencesAdministrees;  // association qualifiée par le nom
+    private HashMap<String, Conference> conferencesAdministrees;  // association qualifiée par le nom
     private Session session;
-    private Map<String, Conference> inscriptions;
-    private Map<String,Communication> communications;
-    private Map<String,Communication> communicationsCorrespondant;
+    private HashMap<String, Conference> inscriptions;
+    private HashMap<Integer,Communication> communications;
+    private HashMap<Integer,Communication> communicationsCorrespondant;
 
     public Utilisateur(String email, String nom, String prenom) {
         assert EmailValidator.getInstance(false, false).isValid(email);
         this.email = email;
         this.nom = nom;
         this.prenom = prenom;
-        //this.conferencesAdministrees = new HashMap<>();
+        this.conferencesAdministrees = new HashMap<>();
+        this.inscriptions = new HashMap<>();
+        this.communications = new HashMap<>();
+        this.communicationsCorrespondant = new HashMap<>();
+        this.session = null;
     }
 
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
+    public boolean addCommunicationEnTantQueCorrespondant(Communication communication) {
+    	this.communicationsCorrespondant.put(communication.getNumero(), communication);
+    	return true;
     }
     
-
-    public void addConferenceAdministree(Conference conference) {
-        assert !this.conferencesAdministrees.containsKey(conf.getNom());
-        this.conferencesAdministrees.put(conference.getNom(), conference);
+    public boolean addCommunicationEnTantQueOrateur(Communication communication) {
+    	this.communications.put(communication.getNumero(), communication);
+    	return true;
     }
-    public void addInscriptionConference(String nomConf, Conference conference ){
+    
+    public boolean addConferenceAdministree(Conference conference) {
+    	this.conferencesAdministrees.put(conference.getNomConf(), conference);
+    	return true;
+    }
+    
+    public boolean addConferenceInscrit(Conference conference) {
+    	this.inscriptions.put(conference.getNomConf(), conference);
+    	return true;
+    }
+    
+    public boolean removeCommunicationEnTantQueCorrespondant(Communication communication) {
+    	this.communicationsCorrespondant.remove(communication.getNumero());
+    	return true;
+    }
+    
+    public boolean removeCommunicationEnTantQueOrateur(Communication communication) {
+    	this.communications.remove(communication.getNumero());
+    	return true;
+    }
+    
+    public boolean setSessionEnTantQueAnimateur(Session session) {
+    	this.session = session;
+    	return true;
+    }
+    
+    public String toString() {
+    	return this.email + this.nom + this.prenom;
+    }
+    
+    
+    
 }
-
-    public void addSessionEnTantQueAnimateur(Session session ){
-
-    }
-    public addCommunicationEnTantQueOrateur(Communication communication){
-        
-    }
-    public removeCommunicationEnTantQueOrateur(Communication communication){
-
-    }
-    public removeSessionEnTantQueAnimateur(Session session){
-
-    }
-    }
